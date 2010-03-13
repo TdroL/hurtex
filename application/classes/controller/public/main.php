9d4167939ed2b5ad65d2afbe1f43552586c20fdc
amos001 <amos_the_great@o2.pl>
2010-03-13 17:26:24 +0100

    tutorial nr1 (Amos)

--- application/classes/controller/public/main.php
+++ application/classes/controller/public/main.php
@@ -4,6 +4,9 @@ class Controller_Public_Main extends Controller_Template
 {
 	public function action_index()
 	{
-		
+		$this->content->wpisy = ORM('Test')
+									->limit(2)
+									->offset(3)
+									->find_all();
 	}
 }
--- application/views/public/main/index.php
+++ application/views/public/main/index.php
@@ -1,4 +1,11 @@
 <?php defined('SYSPATH') or die('No direct script access.'); ?>
 
+<?php foreach($wpisy as $wpis): ?>
+<p>
+	<b><?php echo $wpis->autor ?></b><br />
+	<?php echo $wpis->tresc ?>
+</p><hr />
+<?php endforeach ?>
+
 <p>Plik wyglądu main/index</p>
 <pre>Kawałek kodu w tagach &lt;pre /&gt;</pre>
