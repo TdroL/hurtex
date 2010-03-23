3370d95f04907edfe4ef13443b0b42391e955fbc
TdroL <michal12.mail@gmail.com>
2010-03-23 19:33:44 +0100

    nowy SQL na podstawie ERD

--- schema/projekt.sql
+++ schema/projekt.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Czas wygenerowania: 16 Mar 2010, 17:17
--- Wersja serwera: 5.1.41
--- Wersja PHP: 5.3.1
+-- Czas wygenerowania: 23 Mar 2010, 19:30
+-- Wersja serwera: 5.1.37
+-- Wersja PHP: 5.3.0
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
@@ -18,6 +18,23 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 -- Baza danych: `projekt`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla  `alt_addresses`
+--
+
+CREATE TABLE IF NOT EXISTS `alt_addresses` (
+  `id` int(11) unsigned NOT NULL,
+  `address` text COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Zrzut danych tabeli `alt_addresses`
+--
+
 
 -- --------------------------------------------------------
 
@@ -52,6 +69,8 @@ CREATE TABLE IF NOT EXISTS `clients` (
   `password` char(32) COLLATE utf8_unicode_ci NOT NULL,
   `address` text COLLATE utf8_unicode_ci NOT NULL,
   `phone_number` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `company_name` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `nip` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
@@ -71,8 +90,15 @@ CREATE TABLE IF NOT EXISTS `orders` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `date` int(11) unsigned NOT NULL,
   `client_id` int(11) unsigned NOT NULL,
+  `status` enum('added','accepted','send','canceled') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'accepted',
+  `printed` tinyint(1) NOT NULL DEFAULT '0',
+  `paragon_number` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `invoice` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `address_id` int(11) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `client_id` (`client_id`)
+  UNIQUE KEY `paragon_number` (`paragon_number`,`invoice`),
+  KEY `client_id` (`client_id`),
+  KEY `address_id` (`address_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 --
@@ -114,9 +140,13 @@ CREATE TABLE IF NOT EXISTS `products` (
   `description` text COLLATE utf8_unicode_ci NOT NULL,
   `category_id` int(11) unsigned DEFAULT NULL,
   `unit_id` int(11) unsigned DEFAULT NULL,
+  `quantity` float NOT NULL,
+  `minimal_quantity` float NOT NULL,
+  `price_id` int(11) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `category_id` (`category_id`),
-  KEY `unit_id` (`unit_id`)
+  KEY `unit_id` (`unit_id`),
+  KEY `price_id` (`price_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 --
@@ -133,20 +163,78 @@ CREATE TABLE IF NOT EXISTS `products` (
 CREATE TABLE IF NOT EXISTS `products_orders` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `product_id` int(11) unsigned NOT NULL,
-  `quantity` int(11) unsigned NOT NULL,
+  `quantity` float unsigned NOT NULL,
   `order_id` int(11) unsigned NOT NULL,
   `price_id` int(11) unsigned NOT NULL,
-  `client_id` int(11) unsigned NOT NULL,
-  `confirmed` enum('yes','no') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'no',
   PRIMARY KEY (`id`),
   KEY `order_id` (`order_id`),
   KEY `product_id` (`product_id`),
-  KEY `price_id` (`price_id`),
-  KEY `client_id` (`client_id`)
+  KEY `price_id` (`price_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 --
 -- Zrzut danych tabeli `products_orders`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla  `products_suppliers`
+--
+
+CREATE TABLE IF NOT EXISTS `products_suppliers` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `suppliers_id` int(11) unsigned NOT NULL,
+  `products_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `suppliers_id` (`suppliers_id`),
+  KEY `products_id` (`products_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+--
+-- Zrzut danych tabeli `products_suppliers`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla  `products_supplies`
+--
+
+CREATE TABLE IF NOT EXISTS `products_supplies` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `quantity` float NOT NULL,
+  `product_id` int(11) unsigned NOT NULL,
+  `supply_id` int(11) unsigned NOT NULL,
+  `supplier_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `product_id` (`product_id`,`supply_id`,`supplier_id`),
+  KEY `supply_id` (`supply_id`),
+  KEY `supplier_id` (`supplier_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+--
+-- Zrzut danych tabeli `products_supplies`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla  `product_search`
+--
+
+CREATE TABLE IF NOT EXISTS `product_search` (
+  `product_id` int(11) unsigned NOT NULL,
+  `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `full_data` int(11) NOT NULL,
+  KEY `product_id` (`product_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Zrzut danych tabeli `product_search`
 --
 
 
@@ -159,6 +247,7 @@ CREATE TABLE IF NOT EXISTS `products_orders` (
 CREATE TABLE IF NOT EXISTS `suppliers` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `address` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
@@ -170,20 +259,18 @@ CREATE TABLE IF NOT EXISTS `suppliers` (
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla  `suppliers_products`
+-- Struktura tabeli dla  `supplies`
 --
 
-CREATE TABLE IF NOT EXISTS `suppliers_products` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `suppliers_id` int(11) unsigned NOT NULL,
-  `products_id` int(11) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `suppliers_id` (`suppliers_id`),
-  KEY `products_id` (`products_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+CREATE TABLE IF NOT EXISTS `supplies` (
+  `id` int(11) unsigned NOT NULL,
+  `date` int(11) unsigned NOT NULL,
+  `status` enum('added','in-progress','done') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'added',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Zrzut danych tabeli `suppliers_products`
+-- Zrzut danych tabeli `supplies`
 --
 
 
@@ -196,6 +283,7 @@ CREATE TABLE IF NOT EXISTS `suppliers_products` (
 CREATE TABLE IF NOT EXISTS `units` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `type` enum('integer','float') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'integer',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
@@ -213,6 +301,7 @@ CREATE TABLE IF NOT EXISTS `units` (
 CREATE TABLE IF NOT EXISTS `vats` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `value` float NOT NULL,
+  `name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
@@ -235,6 +324,7 @@ ALTER TABLE `categories`
 -- Ograniczenia dla tabeli `orders`
 --
 ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`address_id`) REFERENCES `alt_addresses` (`id`) ON DELETE SET NULL,
   ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
@@ -248,6 +338,7 @@ ALTER TABLE `prices`
 -- Ograniczenia dla tabeli `products`
 --
 ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_3` FOREIGN KEY (`price_id`) REFERENCES `prices` (`id`),
   ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
   ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`unit_id`) REFERENCES `units` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
@@ -257,12 +348,19 @@ ALTER TABLE `products`
 ALTER TABLE `products_orders`
   ADD CONSTRAINT `products_orders_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `products_orders_ibfk_2` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `products_orders_ibfk_3` FOREIGN KEY (`price_id`) REFERENCES `prices` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `products_orders_ibfk_4` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `products_orders_ibfk_3` FOREIGN KEY (`price_id`) REFERENCES `prices` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Ograniczenia dla tabeli `suppliers_products`
+-- Ograniczenia dla tabeli `products_suppliers`
 --
-ALTER TABLE `suppliers_products`
-  ADD CONSTRAINT `suppliers_products_ibfk_1` FOREIGN KEY (`suppliers_id`) REFERENCES `suppliers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `suppliers_products_ibfk_2` FOREIGN KEY (`products_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `products_suppliers`
+  ADD CONSTRAINT `products_suppliers_ibfk_1` FOREIGN KEY (`suppliers_id`) REFERENCES `suppliers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `products_suppliers_ibfk_2` FOREIGN KEY (`products_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ograniczenia dla tabeli `products_supplies`
+--
+ALTER TABLE `products_supplies`
+  ADD CONSTRAINT `products_supplies_ibfk_3` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`id`),
+  ADD CONSTRAINT `products_supplies_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
+  ADD CONSTRAINT `products_supplies_ibfk_2` FOREIGN KEY (`supply_id`) REFERENCES `supplies` (`id`) ON DELETE CASCADE;
