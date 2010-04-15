7c1b41489d0258876bb38a05e18b6cbabc958279
TdroL <michal12.mail@gmail.com>
2010-04-15 21:58:39 +0200

    Dodano przekierowanie do admina

--- application/classes/controller/public/main.php
+++ application/classes/controller/public/main.php
@@ -4,7 +4,6 @@ class Controller_Public_Main extends Controller_Template
 {
 	public function action_index()
 	{
-		$this->content->products = Jelly::select('product')->execute();
-		$this->content->form = Jelly::factory('product');
+		$this->request->redirect('admin/products');
 	}
 }
