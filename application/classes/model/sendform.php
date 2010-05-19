a0a0e21a23fb324299dc604b52a939dd761b3e82
amos001 <amos_the_great@o2.pl>
2010-05-19 15:22:28 +0200

    zrobiono forme wysylki

--- application/classes/model/sendform.php
+++ application/classes/model/sendform.php
@@ -5,8 +5,7 @@ class Model_Sendform extends Jelly_Model
 
 	public static function initialize(Jelly_Meta $meta)
 	{
-		$meta->table('send_form')
-			->fields(array(
+		$meta->fields(array(
 				'id' => new Field_Primary,
 				'name' => new Field_String(array(
 					'label' => 'Nazwa',
