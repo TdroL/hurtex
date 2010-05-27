d38c7f9d80974229c9dabfff965ccc0e46935c98
kamil_klw <kamil_klw@interia.pl>
2010-05-27 17:17:04 +0200

    blankiet

---
+++ application/views/public/account/payform.php
@@ -0,0 +1,15 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+<!DOCTYPE HTML>
+<html lang="pl-PL">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<?php echo html::style('media/payform.css').PHP_EOL ?>
+</head>
+<body>
+	<div>
+		<table>
+		<tr><td>nazwa firmy</td></tr>
+		</table>
+	</div>
+</body>
